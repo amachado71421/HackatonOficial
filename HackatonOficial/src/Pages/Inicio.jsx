7b692b636af70082f6dbac78a31ae3637224b867
mcoreaabarca1999fwd-hue <mcorea.abarca1999fwd@gmail.com>
2026-03-02 12:40:47 -0600

    commit componentes

--- HackatonOficial/src/Pages/Inicio.jsx
+++ HackatonOficial/src/Pages/Inicio.jsx
@@ -1,9 +1,11 @@
 import InicioLayer1 from "../Components/Inicio/InicioLayer1.jsx";
+import InicioLayer2 from "../Components/Inicio/InicioLayer2.jsx";
 
 function Inicio() {
   return (
     <div>
       <InicioLayer1 />
+      <InicioLayer2 />
     </div>
   );
 }
