e03623506c2c6c301ca589239ba07ad19aa54982
Jeaustin Calderón <tu_correo@gmail.com>
2026-03-02 10:00:45 -0600

    cambios en registro y login

--- HackatonOficial/src/services/Routes.jsx
+++ HackatonOficial/src/services/Routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../Pages/Login";
+import Registro from "../Pages/Registro"; // Importar el nuevo componente
 
 function Routing() {
     return (
         <Router>
             <Routes>
-                <Route path="/Login" element={<Login/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/registro" element={<Registro/>} /> {/* Añadir la nueva ruta */}
             </Routes>
         </Router>
     );
