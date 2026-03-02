2bc78072e7c8824228f04885a9aa62389abc9f5b
mcoreaabarca1999fwd-hue <mcorea.abarca1999fwd@gmail.com>
2026-03-02 14:31:02 -0600

    Actualiza login

--- HackatonOficial/src/services/Routes.jsx
+++ HackatonOficial/src/services/Routes.jsx
@@ -1,7 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inicio from "../Pages/Inicio";
-import Login from "../Pages/Login/Login";
 import Registro from "../Pages/Login/Registro"; // Importar el nuevo componente
+import Login from "../Pages/Registro-Division/Login";
+import Empleados from "../Pages/Registro-Division/Empleados";
+import Administrativo from "../Pages/Registro-Division/Administrativo";
+import Empresas from "../Pages/Registro-Division/Empresas";
+import Pasantes from "../Pages/Registro-Division/Pasantes";
+import Institution from "../Pages/Registro-Division/Institution";
+
 
 function Routing() {
     return (
@@ -10,9 +16,16 @@ function Routing() {
                 <Route path="/" element={<Inicio/>} />
                 <Route path="/Login" element={<Login/>} />
                 <Route path="/registro" element={<Registro/>} /> {/* Añadir la nueva ruta */}
+                <Route path="/Empleados" element={<Empleados/>} />
+                <Route path="/Administrativo" element={<Administrativo/>} />
+                <Route path="/Empresas" element={<Empresas/>} />
+                <Route path="/Pasantes" element={<Pasantes/>} />
+                <Route path="/Institution" element={<Institution/>} />
             </Routes>
         </Router>
     );
 }
 
 export default Routing;
+
+              
