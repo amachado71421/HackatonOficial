a932fa8d711e6a6377d252b161f523835123b3d6
MirandaM <miricruz1570@gmail.com>
2026-03-03 09:04:21 -0600

    cambios

--- HackatonOficial/src/services/Routes.jsx
+++ HackatonOficial/src/services/Routes.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inicio from "../Pages/Inicio";
-import Login from "../Pages/Login/Login";
 import Registro from "../Pages/Login/Registro"; // Importar el nuevo componente
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "../Pages/Login/Login";
+import Login from "../Pages/Registro-Division/Login";
 import Empleados from "../Pages/Registro-Division/Empleados";
 import Administrativo from "../Pages/Registro-Division/Administrativo";
 import Empresas from "../Pages/Registro-Division/Empresas";
@@ -15,7 +13,9 @@ function Routing() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Inicio/>} />
                 <Route path="/Login" element={<Login/>} />
+                <Route path="/registro" element={<Registro/>} /> {/* Añadir la nueva ruta */}
                 <Route path="/Empleados" element={<Empleados/>} />
                 <Route path="/Administrativo" element={<Administrativo/>} />
                 <Route path="/Empresas" element={<Empresas/>} />
